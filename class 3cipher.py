0ae35a1bcee3ea604c8e9ecb4f61d099e1de7b9d
deepakbaliyan2 <115339810+deepakbaliyan2@users.noreply.github.com>
2022-12-01 21:39:14 +0530

    Add files via upload

---
+++ class 3cipher.py
@@ -0,0 +1,16 @@
+#knowing more about print
+
+print("deepak")
+print(1)
+
+
+#print can accept multiple values at once
+print(1, 2, 3, "deepak", 3.4, 1 + 3j)
+
+
+print("hello", end=";")
+print("world")
+
+
+
+print(1, 2, 3, "deepak", 3.4, 1 + 3j, sep=",")
